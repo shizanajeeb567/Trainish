161b2a8f10334a77910a7ba0cdeb10b9e271f8da
Shiza Najeeb <shizanajeeb370@gmail.com>
2025-08-04 16:06:24 +0500

    changed Profile to profile

--- backend/services/profileService.js
+++ backend/services/profileService.js
@@ -1,4 +1,4 @@
-const Profile = require('../models/Profile');
+const Profile = require('../models/profile');
 
 exports.createProfile = async (userId, profileData) => {
   const existing = await Profile.findOne({ where: { userId } });
