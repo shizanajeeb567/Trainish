fcbcf390a8afc3fbebffaaeb5d2d891c67d1f6f7
Shiza Najeeb <shizanajeeb370@gmail.com>
2025-08-04 18:42:59 +0500

    updated db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,13 @@
-const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  port: process.env.DB_PORT || 5432,
-  logging: true, // Optional: enable SQL logging
-});
-
-module.exports = sequelize;
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    logging: false,
+  }
+);
